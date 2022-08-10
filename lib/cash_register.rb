e112d3b6690e30c5f33ab2375bc4be081e66d4dd
Remberto Villar <rembertovillar@gmail.com>
2022-08-10 17:05:14 -0400

    commit tree bf9998599f49c2d630a56496c946ce9da7f1f9f3

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
 
+class CashRegister 
+
+    attr_accessor :total, :discount
+
+    def initialize discount = 0
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item title, price, quantity = 1
+        self.total += 0.98
+        quantity
+    end
+end
